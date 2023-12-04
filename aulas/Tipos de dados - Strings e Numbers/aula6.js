11be2b18fd2a4978de0dca0fe35357d6a2301997
Portela <daniloportela97@gmail.com>
2023-12-04 10:59:39 -0300

    first commit

---
+++ aulas/Tipos de dados - Strings e Numbers/aula6.js
@@ -0,0 +1,6 @@
+const numero1 = 27;
+const numero2 = 23.25;
+const numero3 = -40;
+const total = numero1 + numero2 + numero3;
+
+console.log(total);
---
+++ first-script.js
@@ -0,0 +1,8 @@
+// const msg = "Hello world!"; // O 'const' indica que o conteúdo da variável não pode ser alterado
+let msg = "Hello World!"; // O 'let' indica que o conteúdo da variável pode ser alterado
+
+console.log(msg);
+
+msg = "Hello World mudou!";
+
+console.log(msg);
