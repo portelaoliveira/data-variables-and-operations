f28583db910757d4329c8c039bcaed36ac489cb8
Portela <daniloportela97@gmail.com>
2023-12-04 15:03:53 -0300

    Object.

---
+++ object.js
@@ -0,0 +1,11 @@
+const documents = {
+  nome: "Danilo",
+  sobrenome: "Portela",
+  cpf: "111.111.111-11",
+  empresa: "Rural Tech",
+  idade: 26,
+  brasilero: true,
+};
+
+console.log(documents.empresa);
+console.log(documents["nome"]);
